f719caf808bd78bdd0f91ecda7889d968e8e89f0
Amit <ajangra@ismedusoftsol.com>
2026-05-20 16:53:33 +0530

    Website url open file added

--- src/test/java/com/ihsm/university/ihsmtestcases/flows/websiteurlopen/WebsiteUrlOpen.java
+++ src/test/java/com/ihsm/university/ihsmtestcases/flows/websiteurlopen/WebsiteUrlOpen.java
@@ -72,7 +72,7 @@ public class WebsiteUrlOpen {
         try {
             ChromeOptions options = new ChromeOptions();
 
-            options.addArguments("--headless=new");
+//            options.addArguments("--headless=new");
             options.addArguments("--window-size=1920,1080");
             options.addArguments("--disable-gpu");
             options.addArguments("--no-sandbox");
